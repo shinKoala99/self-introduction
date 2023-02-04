78b3bc21fc8e35963a6f10453cc0691cc94edb68
shinKoala99 <swt.recruit2201@gmail.com>
2023-02-04 16:53:15 +0900

    画面アニメーションの追加と文言修正

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,35 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
   }
 }
+
+gsap.registerPlugin(ScrollTrigger)
+gsap.from('#carrer-content', {
+  scrollTrigger: {
+    trigger: '#carrer-content',
+    start: 'top 80%',
+  },
+  autoAlpha: 0,
+  x: -500
+});
+
+gsap.from('.card', {
+  scrollTrigger: {
+    trigger: '.card',
+    start: 'top 80%',
+  },
+  autoAlpha: 0,
+  y: -200
+});
+
+gsap.from('#note-content', {
+  scrollTrigger: {
+    trigger: '#note-content',
+    start: 'top 80%',
+  },
+  autoAlpha: 0,
+  x: -500
+});
+
+
